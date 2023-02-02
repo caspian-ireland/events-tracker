f3a0a43747db06909b0df30544ff2532bb1f0955
Caspian Ireland <59339201+caspian-ireland@users.noreply.github.com>
2023-02-02 18:03:14 +0000

    feat(simulate_error): dynamic check for codes

--- eventstracker/simulate_errors/__init__.py
+++ eventstracker/simulate_errors/__init__.py
@@ -1,11 +1,57 @@
+"""
+Package for Simulate Errors Blueprint.
+
+This provides routes to make it easy to check status code pages.
+"""
+import typing
 from flask import Blueprint, abort
+from flask import current_app as app
+import re
+import os
 
-simulate_error = Blueprint("simulate_error", __name__)
+
+simulate_error = Blueprint("simulate_error", __name__, url_prefix="/simulate_error")
 
 
-@simulate_error.route("/simulate_error/<int:code>")
-def simulate_error_route(code):
-    if code in [400, 401, 403, 404, 500]:
+def find_supported_codes(templates: typing.List[str]) -> typing.List[int]:
+    """Find Supported Codes.
+
+    Takes a list of template files and finds supported HTTP status codes.
+
+    Args:
+        templates (typing.List[str]): List of template files.
+
+    Returns:
+        typing.List[int]: List of supported HTTP status codes
+    """
+
+    # Find files in templates that are in the directory 'errors',
+    # have a 3 digit number and the .jinja2 extension.
+    error_templates = [
+        i
+        for i in templates
+        if (lambda x: re.search(rf"^errors\{os.sep}\d{{3}}\.jinja2$", x))(i)
+    ]
+
+    # Find the 3 digit code from these files. Split into two steps
+    # with a None check to stop mypy complaining.
+    code_match = [(lambda x: re.search(r"\d{3}", x))(i) for i in error_templates]
+    supported_codes = [int(i.group(0)) for i in code_match if i is not None]
+    return supported_codes
+
+
+@simulate_error.route("/<int:code>")
+def simulate_error_route(code: int):
+    """
+    Route for checking status code pages.
+
+    Dynamic routing used to access available status code pages.
+    If none found then returns 404 page.
+    """
+
+    supported_codes = find_supported_codes(app.jinja_env.list_templates())
+
+    if code in supported_codes:
         abort(code)
     else:
         abort(404)
